b1b30330ce31dc8208ff8c5beabedd13e28a33d2
Shubham <shubhamsingh705010@gmail.com>
2026-07-07 19:02:16 +0530

    main

--- WEEK-3/main.py
+++ WEEK-3/main.py
@@ -2,5 +2,8 @@ def p(c):
     return "<h1>"+c+"</h1>"
 print(p("This is the paragrah"))
 
-def h1():
+
+def h1(c):
+    return "<h1>"+c+"</h1>"
+print(p("This is the h1 tag in html made py python"))
     
