2676d905fd726e91e733082499000df83e0898e9
Shubham <shubhamsingh705010@gmail.com>
2026-07-23 19:54:48 +0530

    mad-1

--- WEEK-3/main.py
+++ WEEK-3/main.py
@@ -1,4 +1,6 @@
-from string import Template
-my_statemenet = Template("Today is $today and tomorrow is $tomorrow.")
-out = my_statemenet.substitute(today = "Modday", tomorrow = "dkjd")
+n = "MAD 1"
+m = "2003"
+
+out = n+m
 print(out)
+print(len(out))
