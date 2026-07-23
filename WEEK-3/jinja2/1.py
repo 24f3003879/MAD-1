c674da4b2b08ad45423a0085b6bb24b7299a04aa
Shubham <shubhamsingh705010@gmail.com>
2026-07-23 18:16:46 +0530

    mad-1

--- WEEK-3/jinja2/1.py
+++ WEEK-3/jinja2/1.py
@@ -1,8 +1,10 @@
 from jinja2 import Template
 
-statement_1 = Template("IIT Madras provides eduction in {{value_1}} {{value_2}}")
+statement_1 = Template("IIT Madras provides eduction in {{value_1}} {{space}} {{value_2}}")
 statement_2 = Template("IIT Madras provides degree in {{value_1}} {{value_2}}")
 out_1 = statement_1.render(value_1 = "Programming")
 out_2 = statement_2.render(value_2 = "Data Science")
+both_12 = statement_1.render(value_1 = "Programming", space = "and", value_2 = "Data Science")
 print(out_1)
 print(out_2)
+print(both_12)
--- WEEK-3/jinja2/app.py
+++ WEEK-3/jinja2/app.py
@@ -1,5 +1,5 @@
 from jinja2 import Template
 
-my_statement = Template("These are the special numbers are:  {% for n in range(11) if n%2 == 0 %} {{n%3}}" "{% endfor %}")
+my_statement = Template("These are the special numbers are:  {% for n in range(11) if n%2 == 0 %} {{n}}" "{% endfor %}")
 out = my_statement.render()
 print(out)
